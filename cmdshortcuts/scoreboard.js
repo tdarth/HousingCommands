ac4ca1302a31eb594af66c28a277c3d9bf63ee00
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 20:23:43 -0700

    Add files via upload

---
+++ cmdshortcuts/scoreboard.js
@@ -0,0 +1,4 @@
+register('command', () => {
+    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aOpening the scoreboard..')
+    ChatLib.command('scoreboard')
+}).setName('sb')
---
+++ commands/al3xisbald.js
@@ -0,0 +1,3 @@
+register('command', () => {
+    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aAl3x is very bald.')
+}).setName('al3xisbald');
