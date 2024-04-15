bcfe0d654f6f4dbcb66bd606f1b688f92a9286ca
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 23:43:55 -0700

    Update mystats.js

--- cmdshortcuts/mystats.js
+++ cmdshortcuts/mystats.js
@@ -1,4 +1,4 @@
 register('command', () => {
-  ChatLib.chat('&8[&b&lHousing&3Commands&8] &aViewing your current Player Stats!')
-  ChatLib.command('viewstats ' + Player.getName());
+    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aViewing your current Player Stats!')
+    ChatLib.command('viewstats ' + Player.getName());
 }).setName('mystats');
