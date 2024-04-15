a5dce4efd7f882c9da53b87c3331fe8fc9092090
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 18:52:49 -0700

    Delete HousingCommands directory

--- HousingCommands/cmdshortcuts/visitingrule.js
+++
@@ -1,4 +0,0 @@
-register('command', (option) => {
-    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aSet visiting rules to ' + option + "!")
-    ChatLib.command('visitingrule ' + option)
-}).setName('vr')
--- HousingCommands/commands/hchelp.js
+++
@@ -1,6 +0,0 @@
-register('command', () => {
-    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aCommand List (1/1)')
-    ChatLib.chat('&a/hchelp &7- View this page.')
-}).setName('hchelp');
-
-// finish this command later
