c88fe855beda902989528428f7b98a8bb2753241
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 19:58:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Commands
 import './commands/hchelp';
 import './commands/al3xisbald';
-import './commands/toggleasterisk'
 
 // cmdshortcuts
 import './cmdshortcuts/eventaction';
@@ -9,9 +8,6 @@ import './cmdshortcuts/functions';
 import './cmdshortcuts/vis';
 import './cmdshortcuts/scoreboard';
 import './cmdshortcuts/visitingrule';
-
-// Features
-import './features/removeasterisk'
 
 ChatLib.chat("")
 ChatLib.chat("&8[&b&lHousing&3Commands&8] &aLoaded! &8(&7/hchelp&8)")
