72ed289bb3e861c713af6a08c8703f019186cd42
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 19:32:38 -0700

    Update toggleasterisk.js

--- commands/toggleasterisk.js
+++ commands/toggleasterisk.js
@@ -1,9 +1,13 @@
+import './features/removeasterisk'
+var removeasterisk == "no"
+
 register('command', () => {
-  if (removeasterisk) {
+  if (removeasterisk == "yes") {
     ChatLib.chat('&8[&b&lHousing&3Commands&8] &aAsterisk enabled!')
-    
+    removeasterisk = "no";
   } else {
     ChatLib.chat('&8[&b&lHousing&3Commands&8] &aAsterisk disabled')
+    removeasterisk = "yes";
   }
 
 }).setName('toggleasterisk');
