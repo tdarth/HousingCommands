3699a4304d61c22ce0bdbd50cbb82300c214fddb
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 23:05:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import './commands/hchelp';
 import './commands/al3xisbald';
 import './commands/coords';
-import './commands/skullpacks';
 
 // cmdshortcuts
 import './cmdshortcuts/eventaction';
@@ -10,6 +9,8 @@ import './cmdshortcuts/functions';
 import './cmdshortcuts/vis';
 import './cmdshortcuts/scoreboard';
 import './cmdshortcuts/visitingrule';
+import './commands/skullpacks';
+import './commands/lobbyhousing';
 
 ChatLib.chat("")
 ChatLib.chat("&8[&b&lHousing&3Commands&8] &aLoaded! &8(&7/hchelp&8)")
