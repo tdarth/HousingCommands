937d211d1b6b5be4f7b8995bfd81812f09a476b8
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 23:11:09 -0700

    Update up.js

--- commands/up.js
+++ commands/up.js
@@ -1,4 +1,8 @@
 register('command', (number) => {
+  if (number) {
     ChatLib.chat('&8[&b&lHousing&3Commands&8] &aTeleported you up ' + number + " blocks!")
     ChatLib.command('tp ~ ' + number + ' ~');
-}).setName('up');
+  } else {
+    ChatLib.chat('&8[&b&lHousing&3Commands&8] &cNo value provided!")
+  }
+}).setName('down');
