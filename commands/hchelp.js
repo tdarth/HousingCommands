55809f4fa0b42dfee33b973e1cba9ca2c8a56e54
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-14 23:03:08 -0700

    Update hchelp.js

--- commands/hchelp.js
+++ commands/hchelp.js
@@ -1,5 +1,5 @@
 register('command', () => {
-    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aCommand List (1/1)')
+    ChatLib.chat('&8[&b&lHousing&3Commands&8] &aVersion: Beta')
     ChatLib.chat('&a/hchelp &7- View this page.')
 }).setName('hchelp');
 
