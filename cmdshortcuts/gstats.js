26fea1c8b3b86d57e2dab0dde30df9a19cf3565c
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-16 16:32:07 -0700

    Delete cmdshortcuts directory

--- cmdshortcuts/gstats.js
+++
@@ -1,6 +0,0 @@
-import Settings from "../features/Settings";
-
-register('command', () => {
-    ChatLib.chat(Settings.chatPrefix + '&aViewing current Global Stats!')
-    ChatLib.command('viewglobalstats')
-}).setName('gstats');
--- cmdshortcuts/mystats.js
+++
@@ -1,6 +0,0 @@
-import Settings from "../features/Settings";
-
-register('command', () => {
-    ChatLib.chat(Settings.chatPrefix + '&aViewing your current Player Stats!')
-    ChatLib.command('viewstats ' + Player.getName());
-}).setName('mystats');
