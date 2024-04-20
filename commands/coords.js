8411b76d0b9402ba2873cae17ecae0cdd056b57e
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-20 09:41:46 -0700

    Delete commands directory

--- commands/coords.js
+++
@@ -1,6 +0,0 @@
-import Settings from "../features/Settings";
-
-register('command', () => {
-    ChatLib.chat(Settings.chatPrefix + '&aX: ' + Math.round(10 * Player.getX() / 10) + " &aY: " + Math.round(10 * Player.getY() / 10) + " Z: " + Math.round(10 * Player.getZ() / 10));
-    Client.showTitle('&f', "&a&l " + Math.round(10 * Player.getX() / 10) + " &a&l " + Math.round(10 * Player.getY() / 10) + " &l " + Math.round(10 * Player.getZ() / 10), 8, 200, 8);
-}).setName('coords');
