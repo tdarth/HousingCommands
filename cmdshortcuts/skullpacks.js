4f21df11a340cd72c80b64ec9c43dc4acb58bf86
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-20 14:07:56 -0700

    Delete cmdshortcuts directory

--- cmdshortcuts/skullpacks.js
+++
@@ -1,6 +0,0 @@
-import Settings from "../features/Settings";
-
-register('command', () => {
-    ChatLib.chat(Settings.chatPrefix + '&aOpening skull packs..')
-    ChatLib.command('skulls')
-}).setName('heads');
