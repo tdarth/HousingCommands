a9f19a9f6df54ab59ffcf6886a7394c5b2b3800d
tdarth <97992436+tdarth@users.noreply.github.com>
2024-04-23 16:20:31 -0700

    Update al3xisbald.js

--- commands/al3xisbald.js
+++ commands/al3xisbald.js
@@ -1,5 +1,13 @@
 import Settings from "../features/Settings";
 
 register('command', () => {
+    ChatLib.chat("&6█████████")
+    ChatLib.chat("&6█████████")
+    ChatLib.chat("&6█████████")
+    ChatLib.chat("&6█████████")
+    ChatLib.chat("&6█&f█&0█&6███&0█&f█&6█ &f &f &f &f &c I'm bald!")
+    ChatLib.chat("&6█&f█&0█&6███&0█&f█&6█ &f &f &f &f &f &c- Al3xWarrior")
+    ChatLib.chat("&6█████████")
+    ChatLib.chat("&6█████████")
     ChatLib.chat(Settings.chatPrefix + '&aAl3x is very bald.')
 }).setName('al3xisbald');
